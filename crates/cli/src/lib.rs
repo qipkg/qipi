9c2a41d553abc132e88fb44a573c357cd1e42f96
nehu3n <developer.nh3@gmail.com>
2025-03-18 19:29:47 -0300

    ✨ feat: Added CLI crate

---
+++ crates/cli/src/lib.rs
@@ -0,0 +1,29 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Args {
+    #[clap(subcommand)]
+    pub cmds: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add {
+        #[clap(short, long)]
+        global: bool,
+
+        packages: Vec<String>,
+    },
+    Remove {
+        #[clap(short, long)]
+        global: bool,
+
+        packages: Vec<String>,
+    },
+    Install {},
+    Uninstall {},
+}
+
+pub async fn init() {
+    let _args = Args::parse();
+}
